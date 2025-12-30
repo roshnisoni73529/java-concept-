94cb322b71caaa822a2141a6a58be5cfd92005e9
Roshni <144519548+roshnisoni73529@users.noreply.github.com>
2025-12-30 17:47:52 +0530

    Update mergesort using array .java

--- mergesort using array .java
+++ mergesort using array .java
@@ -1,1 +1,60 @@
 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    int [] array;
+    int [] tempMergeArr;
+    int length;
+    
+    public static void main(String[] args) {
+        int [] inputArr={48,36,13,52,19,94,21};
+        Main ms = new Main();
+        ms.sort(inputArr);
+        for(int i:inputArr){
+            System.out.print(i+" ");
+        }
+    }
+    public void sort(int inputArr[]){
+        this.array=inputArr;
+        this.length=inputArr.length;
+        this.tempMergeArr=new int[length];
+        divideArray(0,length-1);
+        
+    }
+    public void divideArray(int lowerIndex,int higherIndex){
+        if(lowerIndex<higherIndex){
+            int middle= lowerIndex+(higherIndex-lowerIndex)/2;
+            divideArray(lowerIndex,middle);
+            divideArray(middle+1,higherIndex); 
+            mergeArray(lowerIndex,middle,higherIndex);
+        }
+    }
+    public void mergeArray(int lowerIndex, int middle,int higherIndex){
+        for(int i=lowerIndex;i<=higherIndex;i++){
+            tempMergeArr[i]=array[i];
+        }
+        int i= lowerIndex;
+        int j=middle+1;
+        int k =lowerIndex;
+        while(i<=middle &&  j<=higherIndex){
+            if(tempMergeArr[i]<=tempMergeArr[j]){
+                array[k]=tempMergeArr[i];
+                i++;
+                
+            }
+            else{
+                array[k]=tempMergeArr[j];
+                j++;
+            }
+            k++;
+            
+        }
+        while (i<=middle){
+            array[k]=tempMergeArr[i];
+            k++;
+            i++;
+            
+        }
+    }
+}
